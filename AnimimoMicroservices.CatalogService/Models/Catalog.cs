6a93c8aed9407cba7b0d3a363640bb34759e634b
Mustafa <mustafasaidhelal@gmail.com>
2021-12-17 21:24:23 +0100

    Minor Updates in Code

--- AnimimoMicroservices.CatalogService/Models/Catalog.cs
+++ AnimimoMicroservices.CatalogService/Models/Catalog.cs
@@ -1,13 +1,29 @@
-﻿namespace AnimimoMicroservices.CatalogService
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimimoMicroservices.CatalogService
 {
     public class Catalog
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string? Name { get; set; }
+
+        [Required]
         public string? Description { get; set; }
+
+        [Required]
         public string? imageUrl { get; set; }
+
+        [Required]
         public int Price { get; set; }
+
+        [Required]
         public string? ArticleNumber { get; set; }
+
+        [Required]
         public string? urlSlug { get; set; }
     }
 }
