a713ad6e0421f21fbf87125e98e07ff659cb2396
Mustafa <mustafasaidhelal@gmail.com>
2021-12-20 18:44:30 +0100

    Added Integration to Redis Database and New Service Added to Solution (BasketService)

---
+++ AnimimoMicroservices.NewerBasketService/Controllers/BasketController.cs
@@ -0,0 +1,25 @@
+﻿using AnimimoMicroservices.NewerBasketService.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace AnimimoMicroservices.NewerBasketService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        public BasketController(IDistributedCache cache)
+        {
+            Cache = cache;
+        }
+
+        public IDistributedCache Cache { get; }
+
+        [HttpPost]
+        public IActionResult NewBasket(NewBasketDTO newBasketDto)
+        {
+            return Created("", null); // 201 Status Code
+        }
+    }
+}
---
+++ AnimimoMicroservices.NewerBasketService/Models/DTO/NewBasketDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimimoMicroservices.NewerBasketService.DTO
+{
+    public class NewBasketDTO
+    {
+
+        public string Identifier { get; set; }
+        public Array Items { get; set; }
+    }
+}
